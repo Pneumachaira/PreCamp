bf3b48b36245c1b5202edc0304f56661bb1dd137
Jacob Schuster <thesnake08@hotmail.com>
2021-09-13 13:02:24 -0400

    Initial Commit

---
+++ Schuster_Jacob_JS_Tinkering.js
@@ -0,0 +1,10 @@
+var childHeight = 68;
+function displayIfChildIsAbleToRideTheRollerCoaster(num){
+    if (num > 52) {
+        console.log("Get on that ride, kiddo!");
+        }
+    else {
+        console.log("Sorry kiddo. Maybe next year.");
+        }
+}
+displayIfChildIsAbleToRideTheRollerCoaster(childHeight);
